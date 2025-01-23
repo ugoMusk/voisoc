1d57bb9dcdaf69c4314b412b3bab7a272cd1f12d
ugoMusk <ugobothankgod@gmail.com>
2025-01-23 17:37:19 +0100

    loader utilities

---
+++ frontend/src/components/utils/Loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./loader.css"; // Make sure to link the CSS file
+
+const Loader = () => (
+  <div className="loader-container">
+    <div className="spinner"></div>
+  </div>
+);
+
+export default Loader;
