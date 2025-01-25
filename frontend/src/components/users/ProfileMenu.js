5e5ac93aa551e7c12ec8de92c0180fbad80f8287
ugoMusk <ugobothankgod@gmail.com>
2025-01-25 01:25:44 +0100

    fix syntax errors for profile menu component, refactor database create method to use a simpler approach than transactions

--- frontend/src/components/users/ProfileMenu.js
+++ frontend/src/components/users/ProfileMenu.js
@@ -5,30 +5,34 @@ import { useAuth } from "../contexts/AuthenticationContext.js";
 export default function ProfileMenu({ theme }) {
     const { logout, currentUser } = useAuth();
 
-    return ({ currentUser.username &&
-	      <div className="fixed top-0 z-10 w-full"> {/* Full width and fixed position */}
-		  <div
-		      className={`flex flex-row items-center justify-end p-2 rounded-md ${
+    // Return statement corrected
+    if (!currentUser?.username) {
+        return null; // Render nothing if no current user
+    }
+
+    return (
+        <div className="fixed top-0 z-10 w-full"> {/* Full width and fixed position */}
+            <div
+                className={`flex flex-row items-center justify-end p-2 rounded-md ${
                     theme === "dark" ? "bg-gray-800" : "bg-gray-200"
                 }`}
-		  >
-		      <img
-			  src={currentUser?.bio?.profilePicture}
-			  alt="Profile"
-			  className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border border-gray-300 px-0.5"
-		      />
-		      <button
-			  onClick={logout}
-			  className="text-xs sm:text-sm md:text-base bg-red-500 text-white px-1 py-1 mx-2 rounded hover:bg-red-600"
-		      >
-			  Logout
-		      </button>
-		  </div>
-	      </div>}
-	   );
+            >
+                <img
+                    src={currentUser?.bio?.profilePicture}
+                    alt="Profile"
+                    className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border border-gray-300 px-0.5"
+                />
+                <button
+                    onClick={logout}
+                    className="text-xs sm:text-sm md:text-base bg-red-500 text-white px-1 py-1 mx-2 rounded hover:bg-red-600"
+                >
+                    Logout
+                </button>
+            </div>
+        </div>
+    );
 }
 
 ProfileMenu.propTypes = {
-    profilePicture: PropTypes.string.isRequired,
-    theme: PropTypes.oneOf(["dark", "light"]).isRequired, // Added theme prop
+    theme: PropTypes.oneOf(["dark", "light"]).isRequired, // Added theme prop validation
 };
